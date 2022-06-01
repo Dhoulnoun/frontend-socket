02725d665f6a8866d82b856d031fd267e218f578
Younes Ghoniem <younes.ghoniem@list.lu>
2022-06-01 16:04:01 +0200

    message app done

--- src/App.vue
+++ src/App.vue
@@ -1,81 +1,100 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { io } from "socket.io-client";
+import { onBeforeMount, ref } from "vue";
+
+const socket = io("http://localhost:3001");
+
+const messages = ref([]);
+
+const messageText = ref("");
+
+const joined = ref(false);
+
+const name = ref("");
+
+const typingDisplay = ref("");
+
+onBeforeMount(() => {
+  socket.emit("findAllMessages", {}, (response) => {
+    messages.value = response;
+  });
+
+  socket.on("message", (message) => {
+    messages.value.push(message);
+  });
+
+  socket.on("typing", ({ name, isTyping }) => {
+    if (isTyping) {
+      typingDisplay.value = `${name} is typing...`;
+    } else {
+      typingDisplay.value = "";
+    }
+  });
+});
+const join = () => {
+  socket.emit("join", { name: name.value }, () => {
+    joined.value = true;
+  });
+};
+const sendMessage = () => {
+  socket.emit("createMessage", { text: messageText.value }, () => {
+    // messages.value.push(response)
+    messageText.value = "";
+  });
+};
+
+let timeout;
+const emitTyping = () => {
+  socket.emit("typing", { isTyping: true });
+  timeout = setTimeout(() => {
+    socket.emit("typing", { isTyping: false });
+  }, 3000);
+};
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <div class="chat">
+    <div v-if="!joined">
+      <form @submit.prevent="join">
+        <label> What's your name ?</label>
+        <input v-model="name" />
+        <button type="submit">Send</button>
+      </form>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+    <div class="chat-container" v-else>
+      <div class="messages-container">
+        <div v-for="message in messages">
+          [{{ message.name }}]: {{ message.text }}
+        </div>
+      </div>
+      <div v-if="typingDisplay">{{ typingDisplay }}</div>
+      <hr />
+      <div class="message-input">
+        <form @submit.prevent="sendMessage">
+          <label> Message: </label>
+          <input v-model="messageText" @input="emitTyping" />
+          <button type="submit">Send</button>
+        </form>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style>
-@import './assets/base.css';
+@import "./assets/base.css";
 
-#app {
-  max-width: 1280px;
-  margin: 0 auto;
-  padding: 2rem;
-
-  font-weight: normal;
+.chat {
+  padding: 20px;
+  height: 100vh;
 }
 
-header {
-  line-height: 1.5;
+.chat-container {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-a,
-.green {
-  text-decoration: none;
-  color: hsla(160, 100%, 37%, 1);
-  transition: 0.4s;
-}
-
-@media (hover: hover) {
-  a:hover {
-    background-color: hsla(160, 100%, 37%, 0.2);
-  }
-}
-
-@media (min-width: 1024px) {
-  body {
-    display: flex;
-    place-items: center;
-  }
-
-  #app {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    padding: 0 2rem;
-  }
-
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+.messages-container {
+  flex: 1;
 }
 </style>
